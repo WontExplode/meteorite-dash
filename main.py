7964004e9825315b53a2627a1956137678ab7dd1
Julian Schiebener <j.schiebener@googlemail.com>
2026-05-27 10:45:43 +0200

    simple pygame window

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import pygame
+
+
 def main():
-    print("Hello from meteorite-dash!")
+    pygame.init()
+
+    screen = pygame.display.set_mode((800, 600))
+    pygame.display.set_caption("Meteorite Dash")
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill((10, 10, 20))
+        pygame.display.flip()
+
+    pygame.quit()
 
 
 if __name__ == "__main__":
